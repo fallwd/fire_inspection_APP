46a67b054865652359191a8e742e13e4f9b041de
menghailiang <hailiang.meng@huarui.ai>
2020-04-01 19:16:02 +0800

    提交greendao配置

---
+++ app/src/main/java/com/hr/fire/inspection/utils/GreenDaoHelper.java
@@ -0,0 +1,28 @@
+package com.hr.fire.inspection.utils;
+
+import android.app.Application;
+
+import com.hr.fire.inspection.dao.DaoMaster;
+import com.hr.fire.inspection.dao.DaoSession;
+
+import org.greenrobot.greendao.database.Database;
+
+
+public class GreenDaoHelper extends Application {
+    private DaoSession daoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // regular SQLite database
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "hy_fire_inspection");
+        Database db = helper.getWritableDb();
+
+        daoSession = new DaoMaster(db).newSession();
+    }
+
+    public DaoSession getDaoSession() {
+        return daoSession;
+    }
+}
