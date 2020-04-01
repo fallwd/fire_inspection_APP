7f0f36dcd4bcb8cf3fc0d65ab033ec183254e966
chongyangwang <chongyang.wang@huarui.ai>
2020-04-01 19:29:00 +0800

    登录界面

--- app/src/main/java/com/hr/fire/inspection/activity/LoginActivity.java
+++ app/src/main/java/com/hr/fire/inspection/activity/LoginActivity.java
@@ -1,44 +1,25 @@
 package com.hr.fire.inspection.activity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.hr.fire.inspection.R;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends AppCompatActivity {
     @Override
-    public void widgetClick(View v) {
-
-    }
-
-    @Override
-    public void initParms(Bundle parms) {
-
-    }
-
-    @Override
-    public View bindView() {
-        return null;
-    }
-
-    @Override
-    public int bindLayout() {
-        return R.layout.activity_main;
-    }
-
-    @Override
-    public void initView(View view) {
-
-    }
-
-    @Override
-    public void setListener() {
-
-    }
-
-    @Override
-    public void doBusiness(Context mContext) {
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button login_btn = (Button) this.findViewById(R.id.btnLogin);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
