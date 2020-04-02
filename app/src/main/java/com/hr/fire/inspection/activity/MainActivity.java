6727dccfad12876e87a83a03cd10eb044991dd19
hanliqun <liqun.han@huarui.ai>
2020-04-02 16:28:20 +0800

    增加导航页

--- app/src/main/java/com/hr/fire/inspection/activity/MainActivity.java
+++ app/src/main/java/com/hr/fire/inspection/activity/MainActivity.java
@@ -2,15 +2,31 @@ package com.hr.fire.inspection.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.hr.fire.inspection.R;
 
-public class MainActivity extends AppCompatActivity {
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity {
+    private Button bt1;
+    private Button bt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) this.findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"you clicked button 1",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, NavigationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
