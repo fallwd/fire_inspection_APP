87708db9e3d6bd15deb3a355f7e2071b4907157d
wenbo <18627118090@163.com>
2020-04-10 15:15:46 +0800

    药剂瓶页面调整

--- app/src/main/java/com/hr/fire/inspection/adapter/GoodsAdapter.java
+++ app/src/main/java/com/hr/fire/inspection/adapter/GoodsAdapter.java
@@ -3,6 +3,7 @@ package com.hr.fire.inspection.adapter;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.hr.fire.inspection.R;
@@ -57,6 +59,7 @@ public class GoodsAdapter extends BaseAdapter {
             holder.tv5 = (TextView) convertView.findViewById(R.id.tv5);
             holder.tv6 = (TextView) convertView.findViewById(R.id.tv6);
             holder.tv7 = (TextView) convertView.findViewById(R.id.tv7);
+            holder.rl7 = (RelativeLayout) convertView.findViewById(R.id.rl7);
             holder.tv8 = (TextView) convertView.findViewById(R.id.tv8);
             holder.ev8 = (EditText) convertView.findViewById(R.id.ev8);
             convertView.setTag(holder);
@@ -71,8 +74,12 @@ public class GoodsAdapter extends BaseAdapter {
             holder.tv3.setText("检查内容");
             holder.tv4.setText("合格需求");
             holder.tv5.setText("检查标准");
+            holder.tv6.setCompoundDrawables(null, null, null, null);
+            holder.tv6.setBackground(null);
             holder.tv6.setText("是否合格");
             holder.tv7.setText("现场照片");
+            holder.tv7.setVisibility(View.VISIBLE);
+            holder.rl7.setVisibility(View.GONE);
             holder.tv8.setVisibility(View.VISIBLE);
             holder.ev8.setVisibility(View.GONE);
 
@@ -84,8 +91,16 @@ public class GoodsAdapter extends BaseAdapter {
             holder.tv3.setText("所有存储容器应进行目视检查,看是否有破损生锈或安装的硬件松动的迹象");
             holder.tv4.setText("储瓶无碰撞变形及其他机械性损伤,表面无锈蚀,保护图层完好,涂层颜色为红色");
             holder.tv5.setText("中国船级社《船用消防系统检测机构服务指南》4.10.2");
-            holder.tv6.setText("是否");
-            holder.tv7.setText("照片");
+            holder.tv6.setText("是  ");
+            //在左侧添加图片
+            Drawable drawable = mContext.getResources().getDrawable(R.mipmap.goods_down);
+            drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+            holder.tv6.setCompoundDrawables(null, null, drawable, null);
+            Drawable drawable1 = mContext.getResources().getDrawable(R.drawable.listview_border_margin);
+            holder.tv6.setBackground(drawable1);
+
+            holder.tv7.setVisibility(View.GONE);
+            holder.rl7.setVisibility(View.VISIBLE);
             holder.tv8.setVisibility(View.GONE);
             holder.ev8.setVisibility(View.VISIBLE);
         }
@@ -105,6 +120,7 @@ public class GoodsAdapter extends BaseAdapter {
         TextView tv5;
         TextView tv6;
         TextView tv7;
+        RelativeLayout rl7;
         TextView tv8;
         EditText ev8;
     }
