d13b3713d93dd558c66bf4636da99d01891b933d
wenbo <18627118090@163.com>
2020-04-18 14:05:00 +0800

    1.安卓生成报告获取权限

--- app/src/main/java/com/hr/fire/inspection/activity/NavigationActivity.java
+++ app/src/main/java/com/hr/fire/inspection/activity/NavigationActivity.java
@@ -1,13 +1,16 @@
 package com.hr.fire.inspection.activity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Process;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -28,9 +31,7 @@ public class NavigationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
-        if (Build.VERSION.SDK_INT >= 23) {//6.0才用动态权限
-            initPermission();
-        }
+        initPermission();
 
         LinearLayout btn1 = (LinearLayout) findViewById(R.id.Navigation_Btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -67,13 +68,50 @@ public class NavigationActivity extends AppCompatActivity {
 
     //申请两个权限，录音和文件读写
     //1、首先声明一个数组permissions，将需要的权限都放在里面
-    String[] permissions = new String[]{
+    String[] mPermission = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
-
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.CALL_PHONE};
+    private List<String> mRequestPermission = new ArrayList<>();
+    public static int PERMISSION_REQ = 0x123456;
 
     //权限判断和申请
     private void initPermission() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            for (String one : mPermission) {
+                if (PackageManager.PERMISSION_GRANTED != this.checkPermission(one, Process.myPid(), Process.myUid())) {
+                    mRequestPermission.add(one);
+                }
+            }
+            if (!mRequestPermission.isEmpty()) {
+                this.requestPermissions(mRequestPermission.toArray(new String[mRequestPermission.size()]), PERMISSION_REQ);
+                return;
+            }
+        }
+    }
 
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // 版本兼容
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M) {
+            return;
+        }
+        if (requestCode == PERMISSION_REQ) {
+            for (int i = 0; i < grantResults.length; i++) {
+                for (String one : mPermission) {
+                    if (permissions[i].equals(one) && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        mRequestPermission.remove(one);
+                    }
+                }
+            }
+
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PERMISSION_REQ) {
+            if (resultCode == 0) {
+                Log.d("dong", "=== ? ");
+            }
+        }
     }
 }
